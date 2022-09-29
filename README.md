b1b9d90f6b05473d27e9c2d64795361812c053b2
DanielBerg932 <76453871+DanielBerg932@users.noreply.github.com>
2022-09-29 16:03:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-dbpggo
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-dbpggo)
---
+++ index.ts
@@ -0,0 +1,14 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+var a: number;
+var b: number;
+var c;
+b = 3;
+a = 2;
+c = a + b;
+console.log(c);
